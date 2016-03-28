0676d75eafa1028f514b2f94abb3145eb6de9a27
ctdh24 <ctdh24@gmail.com>
2016-03-27 20:14:44 -0700

    fixing stuff

--- DiscordBot01/DiscordBot01/DankBot.cs
+++ DiscordBot01/DiscordBot01/DankBot.cs
@@ -10,6 +10,7 @@ namespace DiscordBot01
     public class DankBot
     {
         private DiscordClient bot;
+        //DankBot's constructor
         public DankBot()
         {
             bot = new DiscordClient();
@@ -20,7 +21,27 @@ namespace DiscordBot01
 
         private static void Bot_MessageReceived(object sender, MessageEventArgs e)
         {
-            Console.WriteLine("{0} said: {1}", e.User.Name, e.Message.Text);
+            Console.WriteLine("{0} in {1} said: {2}", e.User.Name, e.Channel ,e.Message.Text);
+            if (e.Message.IsAuthor) return;
+            if (e.Message.Text == "1d6")
+            {
+                Random diceroll = new Random();
+                int diceresult = diceroll.Next(1, 7);
+                e.Channel.SendMessage(e.User.Mention + " rolled a " + diceresult);
+            }
+            if (e.Message.Text == "1d20")
+            {
+                Random diceroll = new Random();
+                int diceresult = diceroll.Next(1, 21);
+                e.Channel.SendMessage(e.User.Mention + " rolled a " + diceresult);
+            }
+            if (e.Message.Text == "1d100")
+            {
+                Random diceroll = new Random();
+                int diceresult = diceroll.Next(1, 101);
+                e.Channel.SendMessage(e.User.Mention + " rolled a " + diceresult);
+            }
+
         }
     }
 }
