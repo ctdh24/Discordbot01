525ad68e42ddf6f4550c42feaf278418c806d23f
ctdh24 <ctdh24@gmail.com>
2016-05-25 19:50:41 -0700

    moved code to functions

--- DiscordBot01/DiscordBot01/DankBot.cs
+++ DiscordBot01/DiscordBot01/DankBot.cs
@@ -18,6 +18,42 @@ namespace DiscordBot01
             bot.Connect("discord.bot.tutorial@gmail.com", "september24");
             bot.Wait();
         }
+        private static void diceRoll(MessageEventArgs roll)
+        {
+            Random diceroll = new Random();
+            int temp = roll.Message.Text.IndexOf("d");
+            int dice;
+            try
+            {
+                dice = Int32.Parse(roll.Message.Text.Substring(temp + 1));
+            }
+            catch
+            {
+                roll.Channel.SendMessage(roll.User.Mention + ": Incorrect format");
+                return;
+            }
+            int diceresult = diceroll.Next(1, dice);
+            roll.Channel.SendMessage(roll.User.Mention + " rolled a " + diceresult);
+        }
+
+        private static void PhanKillEmote(MessageEventArgs e)
+        {
+            if (e.Message.Text == "#cute")
+            {
+                e.Channel.SendFile("images/cute.png");
+            }
+            if (e.Message.Text == "#really?")
+            {
+                e.Channel.SendFile("images/annoyed_stare.png");
+            }
+        }
+
+        private static void GOLookUp(MessageEventArgs e)
+        {
+            int temp = e.Message.Text.IndexOf(":");
+            string query = "http://fategrandorder.wikia.com/wiki/" + e.Message.Text.Substring(temp + 1);
+            e.Channel.SendMessage(query);
+        }
 
         private static void Bot_MessageReceived(object sender, MessageEventArgs e)
         {
@@ -26,36 +62,26 @@ namespace DiscordBot01
             //dice rolls
             if (e.Message.Text.Contains("1d") && e.Channel.Name == "dice-check")
             {
-                Random diceroll = new Random();
-                int temp = e.Message.Text.IndexOf("d");
-                int dice;
-                try {
-                    dice = Int32.Parse(e.Message.Text.Substring(temp + 1));
-                }
-                catch {
-                    e.Channel.SendMessage(e.User.Mention + ": Incorrect format");
-                    return;
-                }
-                int diceresult = diceroll.Next(1, dice);
-                e.Channel.SendMessage(e.User.Mention + " rolled a " + diceresult);
+                diceRoll(e);
             }
             //test help list
             if (e.Message.Text == "#help")
             {
                 e.Channel.SendMessage(e.User.Mention + " type 1d followed by a number. Example: 1d20");
             }
-            if(e.Message.Text == "#cute")
+            if (e.Message.Text.Contains("!GOlookup"))
             {
-                e.Channel.SendFile("images/cute.png");
+                GOLookUp(e);
             }
-            if (e.Message.Text == "#really?")
+            else
             {
-                e.Channel.SendFile("images/annoyed_stare.png");
+                PhanKillEmote(e);
             }
+            /*
             if (e.Message.Text == "#doyouunderstand")
             {
                 e.Channel.SendMessage("https://www.youtube.com/watch?v=hWKB1Zxg84s");
-            }
+            }*/
 
 
         }
